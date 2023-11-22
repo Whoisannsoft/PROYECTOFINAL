1f718b4fbd3738f06b7230aa1ba51cebc489f27a
Sofia <soft.ann14@gmail.com>
2023-11-21 22:29:24 -0500

    ultimos cambios por hoy

---
+++ script/user.js
@@ -0,0 +1,12 @@
+export default class User {
+    constructor (name, gender, email, bDay, country, phone, likedPlants, isLogged) {
+        this.name = name;
+        this.gender = gender;
+        this.email = email;
+        this.bDay = bDay;
+        this.country = country;
+        this.phone = phone;
+        this.likedPlants = likedPlants;
+        this.isLogged = isLogged;
+    }
+}
